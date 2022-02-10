7b605708f9ea88496a6bd1ccde3e507a6938b89e
Kravtsov Ivan <57014055+ololo534@users.noreply.github.com>
2022-02-10 12:30:19 +0300

    add callback for def id

--- esp_mqtt/esp_mqtt.ino
+++ esp_mqtt/esp_mqtt.ino
@@ -17,6 +17,7 @@ const int   mqtt_port       = 1883;
 char        Time[]          = "00:00:00";
 char        Date[]          = "00/00/2000";
 int         year_;
+int         id_room;
 byte        last_second, second_, minute_, hour_, day_, month_;
 
 WiFiClient      espClient;
@@ -59,16 +60,25 @@ void setup() {
 void callback(char *topic, byte *payload, unsigned int length) {
     Serial.print("Message arrived in topic: ");
     Serial.println(topic);
-    Serial.print("Message:");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char) payload[i]);
+    Serial.println("Message:");
+
+    StaticJsonDocument <256> doc;
+    deserializeJson(doc, payload);
+
+    if (WiFi.macAddress().c_str() == doc["mac"]) {
+        id_room = doc["id"];
     }
+    char JSONmessageBuffer[256];
+    serializeJsonPretty(doc, JSONmessageBuffer, sizeof(JSONmessageBuffer));
+
+    Serial.println(JSONmessageBuffer);
     Serial.println();
     Serial.println("-----------------------");
 }
 
 void loop() {
     pinMode(2, INPUT);
+
     timeClient.update();
     unsigned long unix_epoch = timeClient.getEpochTime();
     second_ = second(unix_epoch);  
@@ -77,45 +87,48 @@ void loop() {
         // Generate JSON
         StaticJsonDocument<500> doc;
         // id of room
-        doc["id"] = 1;
-        if (last_second != second_) {
-            minute_  = minute(unix_epoch);
-            hour_    = hour(unix_epoch);
-            day_     = day(unix_epoch);
-            month_   = month(unix_epoch);
-            year_    = year(unix_epoch);
+        // IF на ожидание получения id
+        if (id_room != 0) {
+            doc["id"] = id_room;
+            if (last_second != second_) {
+                minute_  = minute(unix_epoch);
+                hour_    = hour(unix_epoch);
+                day_     = day(unix_epoch);
+                month_   = month(unix_epoch);
+                year_    = year(unix_epoch);
 
-            Time[7]  = second_ % 10 + 48;
-            Time[6]  = second_ / 10 + 48;
-            Time[4]  = minute_ % 10 + 48;
-            Time[3]  = minute_ / 10 + 48;
-            Time[1]  = hour_   % 10 + 48;
-            Time[0]  = hour_   / 10 + 48;
+                Time[7]  = second_ % 10 + 48;
+                Time[6]  = second_ / 10 + 48;
+                Time[4]  = minute_ % 10 + 48;
+                Time[3]  = minute_ / 10 + 48;
+                Time[1]  = hour_   % 10 + 48;
+                Time[0]  = hour_   / 10 + 48;
 
-            Date[0]  = day_   / 10 + 48;
-            Date[1]  = day_   % 10 + 48;
-            Date[3]  = month_  / 10 + 48;
-            Date[4]  = month_  % 10 + 48;
-            Date[8]  = (year_   / 10) % 10 + 48;
-            Date[9]  = year_   % 10 % 10 + 48;
+                Date[0]  = day_    / 10 + 48;
+                Date[1]  = day_    % 10 + 48;
+                Date[3]  = month_  / 10 + 48;
+                Date[4]  = month_  % 10 + 48;
+                Date[8]  = (year_  / 10) % 10 + 48;
+                Date[9]  = year_   % 10 % 10 + 48;
+            }
+            last_second = second_;
+            doc["date"] = Date;
+            doc["time"] = Time;
+
+            if (digitalRead(2) == HIGH){
+                doc["move"] = 1;
+            } else {
+                doc["move"] = 0;
+            }
+            char JSONmessageBuffer[300];
+            serializeJsonPretty(doc, JSONmessageBuffer, sizeof(JSONmessageBuffer));
+            Serial.println(JSONmessageBuffer);
+
+            // client.publish(topic, JSONmessageBuffer);
         }
-        last_second = second_;
-        doc["date"] = Date;
-        doc["time"] = Time;
-        if (digitalRead(2) == HIGH){
-            doc["move"] = 1;
-
-        } else {
-            doc["move"] = 0;
-        }
-        char JSONmessageBuffer[300];
-        serializeJsonPretty(doc, JSONmessageBuffer, sizeof(JSONmessageBuffer));
-        Serial.println(JSONmessageBuffer);
-
-        client.publish(topic, JSONmessageBuffer);
     } else {
         Serial.println("Error in WiFi connection");
     }
-    delay(5000);  //Send a request every 20 seconds
+    // delay(5000);  //Send a request every 20 seconds
     client.loop();
 }
